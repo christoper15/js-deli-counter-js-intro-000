3f896fc7e2bcc288ca26a9b33cbe0e2462d75830
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 09:26:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function takeANumber(line , name) {
+  var subArray = [];
+  var array =[];
+
+  subArray.push(name);
+  array.push(`Welcome, ${subArray[subArray.length - 1]}. You are number ${array.length} in line.`)
+
+  }
+  return subArray;
+  return array;
+}

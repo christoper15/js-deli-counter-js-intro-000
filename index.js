0bcece57384e335f033f3673abc1fcc243665096
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 13:18:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
     var string = `The line is currently: 1. ${line[0]}`
 
     let i = 0;
-    while (i < line.length) {
+    while (i < line.length-1) {
     string = string + `, ${i+2}. ${line[i+1]}`
     i++;
     }

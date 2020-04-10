036d8e6471080ec05a4d082d42e9106c40f099f3
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 09:27:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 function takeANumber(line , name) {
-  var subArray = [];
-  var array =[];
-
-  subArray.push(name);
-  array.push(`Welcome, ${subArray[subArray.length - 1]}. You are number ${array.length} in line.`)
-
-  }
-  return subArray;
-  return array;
+return `${name} and ${line}`
 }

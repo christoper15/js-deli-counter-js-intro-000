f4fca92bc68c0863abb61ddbfee6f7a67b7c275c
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 10:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ return `Welcome, ${name}. You are number ${line.length} in line.`;
 
 function nowServing(line) {
   var firstperson = line.splice[1];
-  if (line.length = 0) {
-    return "There is nobody waiting to be served";
-  } else {
+  if (line.length > 0) {
     line.shift()
     return `Currently serving ${firstperson}.`;
-
+  } else {
+    return "There is nobody waiting to be served";
   }
 }
